35c8eac60d6410112b7b9d48a7a7c4481a074649
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2023-05-26 12:12:00 +0600

    Update content.js
    Added unread message count support on the tab title to know when any unread message is found

--- content.js
+++ content.js
@@ -8,9 +8,14 @@ fetch("https://www.fiverr.com/inbox/counters")
   .then((data) => {
     var stat = JSON.parse(data);
     var spam_count = stat.spam;
+    var unread_count = stat.unread;
     var dashboard = document.querySelector('.seller-main-item');
+    var title = document.querySelector('title');
     dashboard.innerHTML = dashboard.innerHTML + ' &nbsp;<span style="color:red">(' + spam_count + ' spam)</span>';
+    if (unread_count > 0){
+      title.innerHTML = title.innerHTML + ' &nbsp;[' + unread_count + ' messages]';
+    }
   })
   .catch((error) => {
-    // console.error(error);
+    // console.log(error);
   });
